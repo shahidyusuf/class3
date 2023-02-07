9c64fed5706293b7453a06880044429f898f3ee4
Muhammad Shahid Yusuf <shd.92yus@gmail.com>
2023-02-07 07:52:51 +0500

    update.home

---
+++ java.js
@@ -0,0 +1,12 @@
+// var num = prompt ("Type a number");
+
+// if (num % 3 === 0 && num % 5 ===0){
+//     console.log("Both 3 & 5");
+// }
+// else if (num % 3 === 0){
+//     console.log("only 3");
+// }
+// else if (num % 5 ===0){
+//     console.log("only 5");
+// }
+console.log(undefined && false);
