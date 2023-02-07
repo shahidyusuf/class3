5ed55cb874e698472a06c4169fe8fd9b9286de4f
Shahid Yusuf <shd.92yus@gmail.com>
2023-02-07 20:57:58 +0500

    update.shop

--- java.js
+++ java.js
@@ -9,4 +9,8 @@
 // else if (num % 5 ===0){
 //     console.log("only 5");
 // }
-console.log(undefined && false);
+// console.log(undefined && false);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false || true)
+console.log(null || true)
