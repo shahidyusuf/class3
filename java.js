a494ad6b017e5807bcb9c439ddc6ac0342e85fe3
Shahid Yusuf <shd.92yus@gmail.com>
2023-02-08 20:55:13 +0500

    update.shop

--- java.js
+++ java.js
@@ -22,6 +22,50 @@
 // console.log(null || undefined);
 // console.log(null && true || false);
 // console.log(null && false|| false && true);
-console.log(null && true || true && 1);
-console.log(null && true) || (true && 1);
+// console.log(null && true || true && 1);
 // console.log(null && true) || (true && 1);
+// console.log(null && true) || (true && 1);
+
+// Example
+
+// var num = +prompt("Type Your Percentage");
+// var grade;
+// if (num >= 0 && num <=100){
+// if (num >= 90) {
+    // grade = "A+";
+// } else if (num >= 80) {
+    // grade = "A";
+// } else if (num >= 70) {
+    // grade = "B";
+// } else if (num >= 60) {
+    // grade = "C";
+// } else if (num >= 50) {
+    // grade ="D"
+// } else {
+    // grade = "Faile";
+// }
+// console.log(grade);
+// } else {
+    // console.log("Wrong Number")
+// }
+
+var months = ["January", "Feburary"];
+console.log(months);
+console.log(months[0]);
+console.log(months[1]);
+
+var month = ['January', 'Feburary'];
+month[2] = 'March';
+console.log(month);
+
+var month = ['January', 'Feburary'];
+month[0] = 'Years';
+console.log(month);
+
+var monthss = ['Jan', 'Feb'];
+monthss.push('Apr');
+console.log(monthss);
+
+var months = ['jan', 'feb','mar'];
+months.pop();
+console.log(months)
